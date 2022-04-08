1b79d7a2773a24525301de95d1bb85e30e8a7e6b
Nicolas Bonnefont <nicolas.7l@hotmail.com>
2022-04-08 20:50:11 -0300

    acerto

--- ATUALIZADOR.bat
+++ ATUALIZADOR.bat
@@ -1,5 +1,5 @@
 @echo off
-msiexec "C:\utilitario-bkp-mor\instaladores\ATUALIZADOR.msi" /quiet 
+msiexec C:\utilitario-bkp-mor\instaladores\ATUALIZADOR.msi /quiet 
 git pull
 npm run install-pacotes
 
