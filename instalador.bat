72b7f8b3ec590e97a94d6c8bbe5d678739d9d2e9
Nicolas Bonnefont <nicolas.7l@hotmail.com>
2022-04-08 16:51:23 -0300

    melhorias nos fontes

---
+++ instalador.bat
@@ -0,0 +1,4 @@
+@echo off
+git pull
+npm run install-pacotes
+pause
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import 'dotenv/config'
 import Excluir from './ExcluirArquivos/index.js'
 import Monitorar from './MonitoraArquivos/index.js'
 
-
 if (!process.env.ID_EMPRESA || !process.env.QTDE_MES_BKP ||
   !process.env.DIRETORIO_BKP || !process.env.USUARIO ||
   !process.env.SENHA) {
